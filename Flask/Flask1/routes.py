4c1c1d00750180082f1f8c86ff686d1e11eca344
Dan Bal <db3260@columbia.edu>
2018-11-03 15:33:52 +0000

    created v1 of account page, error pops up when updating the picture, should be fixed in v2.

--- Flask/Flask1/routes.py
+++ Flask/Flask1/routes.py
@@ -1,6 +1,8 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from Flask.Flask1 import app, db, bcrypt
-from Flask.Flask1.Account_form import RegistrationForm, LoginForm
+from Flask.Flask1.Account_form import RegistrationForm, LoginForm, UpdateAccountForm
 from Flask.Flask1.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -80,13 +82,34 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.route_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
 # Account page route
 
 
-@app.route("/account")
+@app.route("/account", methods=["GET", 'POST'])
 @login_required
 # Returning user's data to see on Account page
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.image_file = form.image_file.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
