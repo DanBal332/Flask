063d63615ad17ce85f8263956c5bb7b12e582d0a
Dan Bal <db3260@columbia.edu>
2018-11-20 17:37:08 +0000

    Login page updated

--- Flask/Flask1/main/routes.py
+++ Flask/Flask1/main/routes.py
@@ -13,11 +13,11 @@ def home():
     return render_template('home.html', posts=posts)
 
 
-# @main.route("/blog")
-# def blog():
-#     page = request.args.get('page', 1, type=int)
-#     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-#     return render_template('home.html', posts=posts)
+@main.route("/blog")
+def blog():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('blog.html', posts=posts)
 
 # About page route
 
